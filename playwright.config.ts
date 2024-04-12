68960bde81a1f0a33594a67d56ad40c5a25b05b6
alessandrror <ssandrorivas@gmail.com>
2024-04-12 02:32:15 -0600

    🤡 Modifying: port for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -121,8 +121,8 @@ export default defineConfig({
      * Use the preview server on CI for more realistic testing.
      * Playwright will re-use the local server if there is already a dev-server running.
      */
-    command: process.env.CI ? 'vite preview --port 5173' : 'vite dev',
-    port: 5173,
+    command: process.env.CI ? 'vite preview --port 4173' : 'vite dev',
+    port: 4173,
     reuseExistingServer: !process.env.CI
   }
 })
